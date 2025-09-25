d936b2eaead7342bfa5329f0529c60371e5e132d
Akul Thayyil <thayyilakul99@gmail.com>
2025-09-25 22:52:15 +0530

    modified database seeder line

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(PostSeeder::class);
+        $this->call([RestaurantSeeder::class, OrderSeeder::class]);
     }
 }
